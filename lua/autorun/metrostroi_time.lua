036094249127154421b48a0a9da4288bcc902fac
Alexell <alexell@alexell.ru>
2019-10-08 15:35:56 +0300

    Удалено уже не актуальное условие.

--- lua/autorun/metrostroi_time.lua
+++ lua/autorun/metrostroi_time.lua
@@ -1,8 +1,4 @@
 -- переопределяем функцию и поправляем время
 Metrostroi.GetSyncTime = function(notsync)
-    if notsync then
-        return os.time() - Metrostroi.GetTimedT(notsync)
-    else
-        return os.time() - Metrostroi.GetTimedT(notsync) + 10800
-    end
+	return os.time() - Metrostroi.GetTimedT(notsync) + 10800
 end
