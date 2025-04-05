9a37efbf2d1bd7145490ec546dbe57a573be1836
Lufou <lucas.damase@laposte.net>
2025-04-05 22:32:07 +0200

    Better performances
    Isn't it better optimization with mat_specular 0 ?

--- lua/metrostroi_advanced/cl_metrostroi_advanced.lua
+++ lua/metrostroi_advanced/cl_metrostroi_advanced.lua
@@ -102,7 +102,7 @@ local function ClientOptimize(opt)
 	if (tonumber(opt) == 2) then
 		RunConsoleCommand("gmod_mcore_test",1)
 		RunConsoleCommand("mat_queue_mode",2)
-		RunConsoleCommand("mat_specular",1)
+		RunConsoleCommand("mat_specular",0)
 		RunConsoleCommand("cl_threaded_bone_setup",1)
 		RunConsoleCommand("cl_threaded_client_leaf_system",1)
 		RunConsoleCommand("r_threaded_client_shadow_manager",1)
